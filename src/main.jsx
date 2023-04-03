bde312343cf0dfd75138a4d53b90eebd21c4322d
webashraf <aliashraftamim@gmail.com>
2023-04-03 16:52:37 +0600

    add router error page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Error from "./components/Error/Error";
 import Product from "./components/Product/Product";
 import ProductDetails from "./components/ProductsDetails/ProductDetails";
 import "./index.css";
@@ -11,25 +12,31 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    errorElement: <Error></Error>,
 
     children: [
       {
         path: "/about-us",
         element: <About></About>,
+        errorElement: <Error></Error>,
       },
       {
         path: "/contact-us",
         element: <Contact></Contact>,
+        errorElement: <Error></Error>,
       },
       {
         path: "/product",
         element: <Product></Product>,
         loader: () => fetch("https://jsonplaceholder.typicode.com/users"),
+        errorElement: <Error></Error>,
       },
       {
         path: "/products/:Id",
         element: <ProductDetails></ProductDetails>,
-        loader: ({ params }) => fetch(`https://jsonplaceholder.typicode.com/users/${params.Id}`),
+        loader: ({ params }) =>
+          fetch(`https://jsonplaceholder.typicode.com/users/${params.Id}`),
+        errorElement: <Error></Error>,
       },
     ],
   },
